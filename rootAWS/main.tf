9d979bb5afeb3efc63b865e223ee897ab04c4413
Pablo <pcisnerp@gmail.com>
2024-02-01 21:34:16 -0800

    Add AWS IAM access key for admin user

--- rootAWS/main.tf
+++ rootAWS/main.tf
@@ -10,3 +10,7 @@ resource "aws_iam_user_policy_attachment" "admin_policy_attachment" {
   user       = aws_iam_user.admin_user.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+
+resource "aws_iam_access_key" "admin_user_key" {
+  user    = aws_iam_user.admin_user.name
+}
