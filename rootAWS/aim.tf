fa9be193f56fd8964a4a924beccc844271b56778
Pablo <pcisnerp@gmail.com>
2024-02-02 11:37:35 -0800

    Add AWS IAM user policy attachments

---
+++ rootAWS/aim.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_user_policy_attachment" "admin_policy_attachment" {
+  user       = aws_iam_user.admin_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+/* resource "aws_iam_user_policy_attachment" "admin_policy_attachment" {
+  user       = aws_iam_user.admin_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "admin_policy_attachment" {
+  user       = aws_iam_user.admin_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "admin_policy_attachment" {
+  user       = aws_iam_user.admin_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "admin_policy_attachment" {
+  user       = aws_iam_user.admin_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "admin_policy_attachment" {
+  user       = aws_iam_user.admin_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
+} */
