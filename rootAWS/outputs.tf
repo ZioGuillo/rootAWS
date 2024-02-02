248be2b58411fea387c8882b8f2e4872eff1fa49
Pablo <pcisnerp@gmail.com>
2024-02-01 21:41:46 -0800

    Add outputs for admin user access key ID and secret access key

--- rootAWS/outputs.tf
+++ rootAWS/outputs.tf
@@ -7,3 +7,12 @@ output "admin_user_name" {
   description = "The name of the created IAM user with administrative privileges"
   value       = aws_iam_user.admin_user.name
 }
+
+output "admin_user_access_key_id" {
+  value = aws_iam_access_key.admin_user_key.id
+}
+
+output "admin_user_secret_access_key" {
+  value     = aws_iam_access_key.admin_user_key.secret
+  sensitive = true # Mark this output as sensitive to prevent it from being displayed in logs
+}
