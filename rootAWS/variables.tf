a737bdce5618135ed80b4f4f6c3886ac79048243
Pablo <pcisnerp@gmail.com>
2024-02-08 17:06:09 -0800

    Add variables for S3 bucket and DynamoDB table

--- rootAWS/variables.tf
+++ rootAWS/variables.tf
@@ -10,3 +10,17 @@ variable "password" {
   description = "The password for the AWS IAM user with administrative privileges"
   type        = string
 }
+
+variable "bucket" {
+  description = "The name of the S3 bucket for storing Terraform state."
+  type        = string
+}
+
+variable "key" {
+  description = "The name of the Terraform state file."
+  type        = string
+}
+variable "dynamodb_table" {
+  description = "The name of the DynamoDB table for state locking."
+  type        = string
+}
